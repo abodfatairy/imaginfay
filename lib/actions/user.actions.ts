f669265f1a4c1703df97d9add1ef95be7fc4d91a
abodfatairy <abodfatairy@gmail.com>
2024-03-13 20:33:15 +0200

    third commit

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,8 +1,7 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
-
-import User from "../database/models/user.model";
+import User from "../dataBase/models/user.model"
 import { connectToDatabase } from "../database/mongoose";
 import { handleError } from "../utils";
 
